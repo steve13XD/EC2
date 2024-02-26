7a95990d19d93a9f69c2e9572955e108c8f463d5
mosheline <moshe.felixg@gmail.com>
2024-02-25 22:00:41 -0500

    Diseñando Recylcer View

--- app/src/main/java/pe/edu/idat/evaluacioncontinua2/ListaActivity.kt
+++ app/src/main/java/pe/edu/idat/evaluacioncontinua2/ListaActivity.kt
@@ -2,10 +2,13 @@ package pe.edu.idat.evaluacioncontinua2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.edu.idat.evaluacioncontinua2.databinding.ActivityListaBinding
 
 class ListaActivity : AppCompatActivity() {
+    lateinit var binding: ActivityListaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista)
+        binding = ActivityListaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
