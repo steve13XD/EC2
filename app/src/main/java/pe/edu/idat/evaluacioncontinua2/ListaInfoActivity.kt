b302800b4dea8c9fef958464ed7542ebe8c32e05
Jossep25 <lolitoscomor@gmail.com>
2024-02-26 13:51:18 -0500

    Interfaz de listado y funcion terminado

---
+++ app/src/main/java/pe/edu/idat/evaluacioncontinua2/ListaInfoActivity.kt
@@ -0,0 +1,30 @@
+package pe.edu.idat.evaluacioncontinua2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
+import pe.edu.idat.evaluacioncontinua2.R
+import pe.edu.idat.evaluacioncontinua2.databinding.ActivityListaInfoBinding
+
+class ListaInfoActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding: ActivityListaInfoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListaInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbar.setOnClickListener(this)
+        var listaInfo =intent.getSerializableExtra("listaInfo") as ArrayList<String>
+        val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, listaInfo)
+        binding.lvInfo.adapter = adapter
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.toolbar -> {
+                onBackPressed()
+            }
+        }
+    }
+}
--- app/src/main/java/pe/edu/idat/evaluacioncontinua2/RegistroActivity.kt
+++ app/src/main/java/pe/edu/idat/evaluacioncontinua2/RegistroActivity.kt
@@ -1,5 +1,6 @@
 package pe.edu.idat.evaluacioncontinua2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,8 @@ import pe.edu.idat.evaluacioncontinua2.util.TipoMensaje
 
 class RegistroActivity : AppCompatActivity(), View.OnClickListener{
     private lateinit var binding: ActivityRegistroBinding
+    private val listaInfo = ArrayList<String>()
+    private val listaCualidades = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistroBinding.inflate(layoutInflater)
@@ -19,6 +22,7 @@ class RegistroActivity : AppCompatActivity(), View.OnClickListener{
         binding.cbpuntual.setOnClickListener(this)
         binding.cbrespetuoso.setOnClickListener(this)
         binding.cbresponsable.setOnClickListener(this)
+        binding.btnlistainfo.setOnClickListener(this)
     }
 
     fun validarcualidades():Boolean{
@@ -62,7 +66,6 @@ class RegistroActivity : AppCompatActivity(), View.OnClickListener{
         val carreraValida = validarcarrera()
         val cualidadesValidas = validarcualidades()
         if (camposValidos && carreraValida && cualidadesValidas) {
-            AppMensaje.noti(binding.root, getString(R.string.notificacion), TipoMensaje.CORRECTO)
             return true
         } else {
             var mensajeError = ""
@@ -77,13 +80,58 @@ class RegistroActivity : AppCompatActivity(), View.OnClickListener{
             return false
         }
     }
+    private fun registrarInfo(){
+        if(validarFormulario()){
+            var infor = "Nombre: " +binding.edNombre.text.toString()+"\nApellido: "+
+                    binding.edApellido.text.toString()+"\nDni: "+
+                    binding.edDni.text.toString()+"\nNumero: "+
+                    binding.edCelular.text.toString()+"\nEmail: "+
+                    binding.edEmail.text.toString()+"\nCualidad: "+
+                    obtenerCualidades()+"\nCarrera: "+
+                    obtenerCarrera()
+            listaInfo.add(infor)
+            AppMensaje.noti(binding.root, getString(R.string.notificacion), TipoMensaje.CORRECTO)
+        }
+    }
+    fun obtenerCualidades():String{
+        val cualidadesSeleccionadas = mutableListOf<String>()
+
+        if (binding.cbpuntual.isChecked) {
+            cualidadesSeleccionadas.add(binding.cbpuntual.text.toString())
+        }
+        if (binding.cbrespetuoso.isChecked) {
+            cualidadesSeleccionadas.add(binding.cbrespetuoso.text.toString())
+        }
+        if (binding.cbresponsable.isChecked) {
+            cualidadesSeleccionadas.add(binding.cbresponsable.text.toString())
+        }
+        if (binding.cbotros.isChecked) {
+            val otrosText = binding.edOtros.text.toString().trim()
+            if (otrosText.isNotEmpty()) {
+                cualidadesSeleccionadas.add(otrosText)
+            }
+        }
+        return cualidadesSeleccionadas.joinToString(separator = ", ")
+    }
+    fun obtenerCarrera():String{
+        return when (binding.radioGroup.checkedRadioButtonId) {
+            R.id.rbingenieria -> binding.rbingenieria.text.toString()
+            R.id.rbciencias -> binding.rbciencias.text.toString()
+            R.id.rbadministracion -> binding.rbadministracion.text.toString()
+            R.id.rbotros -> binding.rbotros.text.toString()
+            else -> "Ninguna carrera seleccionada"
+        }
+    }
+    private fun irlistainfo(){
+        var intent = Intent(applicationContext, ListaInfoActivity::class.java).apply {
+            putExtra("listaInfo", listaInfo)
+        }
+        startActivity(intent)
+    }
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.btnAcceder -> {
-                if (validarFormulario()) {
-
-                }
-            }
+            R.id.btnlistainfo -> irlistainfo()
+            R.id.btnAcceder -> registrarInfo()
             R.id.toolbar -> {
                 onBackPressed()
             }
